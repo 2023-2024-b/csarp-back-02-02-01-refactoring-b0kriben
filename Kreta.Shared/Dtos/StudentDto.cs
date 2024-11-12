5d13ad8f1221d7b9631d97966816c386d9d537ea
Krisztin Benedek <krisztinbeni@gmail.com>
2024-11-12 10:12:35 +0100

    a StudentDto-ba egyenlőre nem kell constructor

--- Kreta.Shared/Dtos/StudentDto.cs
+++ Kreta.Shared/Dtos/StudentDto.cs
@@ -3,39 +3,6 @@ namespace Kreta.Shared.Dtos
 {
     public class StudentDto
     {
-        public StudentDto(Guid id, string firstName, string lastName, DateTime birthsDay, int schoolYear, SchoolClassType schoolClass, string educationLevel, bool isWooman)
-        {
-            Id = id;
-            FirstName = firstName;
-            LastName = lastName;
-            BirthsDay = birthsDay;
-            SchoolYear = schoolYear;
-            SchoolClass = schoolClass;
-            EducationLevel = educationLevel;
-            IsWoman = isWooman;
-        }
-        public StudentDto(string firstName, string lastName, DateTime birthsDay, int schoolYear, SchoolClassType schoolClass, string educationLevel, bool isWooman)
-        {
-            Id = new Guid();
-            FirstName = firstName;
-            LastName = lastName;
-            BirthsDay = birthsDay;
-            SchoolYear = schoolYear;
-            SchoolClass = schoolClass;
-            EducationLevel = educationLevel;
-            IsWoman = isWooman;
-        }
-        public StudentDto()
-        {
-            Id = new Guid();
-            FirstName = string.Empty;
-            LastName = string.Empty;
-            BirthsDay = new DateTime();
-            SchoolYear = 9;
-            SchoolClass = SchoolClassType.ClassA;
-            EducationLevel = string.Empty;
-            IsWoman = false;
-        }
         public Guid Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
